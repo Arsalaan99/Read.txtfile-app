b6ebae0bfe871449c1ed5db22e02228c2f227023
rahil <rahilkazi66@gmail.com>
2021-02-17 17:57:15 +0530

    i1: plotting data using graphview library, a lot of work to be done this is iteration 1

--- app/src/main/java/com/freecsarsalaan99/myapplication/MainActivity.java
+++ app/src/main/java/com/freecsarsalaan99/myapplication/MainActivity.java
@@ -3,9 +3,11 @@ package com.freecsarsalaan99.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,16 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Double> ecgdata;
 
-    TextView textView;
+    GraphView graph;
+
+    Double xVal = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text1);
 
-        textView.setMovementMethod(new ScrollingMovementMethod());
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
         InputStream is = this.getResources().openRawResource(R.raw.output);
         // String data = "";
 
@@ -38,20 +43,40 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             String line;
+            ArrayList<DataPoint> arrDataPoint=new ArrayList<>();
+
             while ((line = br.readLine()) != null) {
                 text.append(line);
                 Log.i("Test", "text : " + text + " :end");
                 text.append('\n');
                 ecgdata.add(Double.parseDouble(line));
+                xVal = xVal + 1;
+
+                DataPoint dp = new DataPoint(xVal, Double.parseDouble(line));
+                arrDataPoint.add(dp);
             }
             br.close();
+            DataPoint[] listDp = new DataPoint[arrDataPoint.size()];
+            for(int i=0;i<arrDataPoint.size();i++){
+                listDp[i]=arrDataPoint.get(i);
+            }
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(listDp);
+            graph.addSeries(series);
             Log.i("Float","ecg data in floating point numbers" + ecgdata);
             Log.i("Float", "the size of array "+ecgdata.size());
+            Log.i("Float", "the size"+xVal);
+            Log.i("Float", "the points"+arrDataPoint);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        textView.setText(text.toString());
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(100);
+
+        graph.getViewport().setScrollable(true); // enables horizontal scrolling
+        graph.getViewport().setScrollableY(true); // enables vertical scrolling
+
 
     }
 }
