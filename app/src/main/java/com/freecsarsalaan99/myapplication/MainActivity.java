7c17f6435f5ea9d3daf4034aab5b22ded229f895
rahil <rahilkazi66@gmail.com>
2021-02-17 15:09:58 +0530

    storing the data in an arrayList of doubles

--- app/src/main/java/com/freecsarsalaan99/myapplication/MainActivity.java
+++ app/src/main/java/com/freecsarsalaan99/myapplication/MainActivity.java
@@ -11,9 +11,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Double> ecgdata;
 
     TextView textView;
 
@@ -28,20 +30,23 @@ public class MainActivity extends AppCompatActivity {
         InputStream is = this.getResources().openRawResource(R.raw.output);
         // String data = "";
 
+        ecgdata = new ArrayList<>();
+
         //StringBuffer sbuffer = new StringBuffer();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         StringBuilder text = new StringBuilder();
         try {
-
 
             String line;
             while ((line = br.readLine()) != null) {
                 text.append(line);
                 Log.i("Test", "text : " + text + " :end");
                 text.append('\n');
-
+                ecgdata.add(Double.parseDouble(line));
             }
             br.close();
+            Log.i("Float","ecg data in floating point numbers" + ecgdata);
+            Log.i("Float", "the size of array "+ecgdata.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
